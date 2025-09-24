7668a38c33e3200596584f36b05a4da6f318a122
James Bole Pan <jamespan1118@hotmail.com>
2025-09-23 22:09:41 -0400

    updated the f.Params initialization to be more readable

--- check_dx.py
+++ check_dx.py
@@ -9,8 +9,31 @@ x0_acc = np.array([
     [[10e6,10e6,0.0029,0.02,0.015], [10e6,10e6,0.0029,0.02,0.015]],
     [[10e6,10e6,0.0029,0.02,0.015], [10e6,10e6,0.0029,0.02,0.015]]
 ])
-p_acc = f.Params(0.5, 5e7, 2, 3e-7, 0.1, 10, 0.18, 0.01, 0.03, 3e-7, 12.6, 0.1, 0.01, 2.5e-8, 
-                 18, 0.01, 0.03, 0.05, 0.01, 1, 2, 2)
+
+p_acc = f.Params(
+    lc=0.5,        # lambda_c
+    tc=5e7,        # theta_c
+    nc=2,          # n_c
+    k8=3e-7,       # kappa_8
+    ng=0.1,        # n_g
+    ki=10,         # K_i
+    dc=0.18,       # d_c
+    D_c=0.01,      # D_c
+    lt8=0.03,      # lambda_t8
+    rl=3e-7,       # rho_l
+    kq=12.6,       # K_q
+    dt8=0.1,       # d_t8
+    D_t8=0.01,     # D_t8
+    ligt8=2.5e-8,  # lambda_igt8
+    dig=18,        # d_ig
+    D_ig=0.01,     # D_ig
+    mu_a=0.03,     # mu_a
+    da=0.05,       # d_a
+    D_a=0.01,      # D_a
+    rows=2,        # rows in grid
+    cols=2         # cols in grid
+)
+
 u_acc = 0.015
 
 # plot to study error on Finite Difference Jacobian for different dx
